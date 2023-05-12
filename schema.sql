06040d0543bf9b235f2d0260942cc20c82da190c
Synora Eusebio <synora.eusebio@gmail.com>
2023-05-12 09:14:16 -0400

    Deployment

--- schema.sql
+++ schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE todolists (
-    id serial PRIMARY KEY,
-    title text NOT NULL UNIQUE
-    username text NOT NULL, 
+  id serial PRIMARY KEY,
+  title text NOT NULL UNIQUE,
+  username text NOT NULL
 );
 
 CREATE TABLE todos (
-    id serial PRIMARY KEY,
-    title text NOT NULL, 
-    done boolean NOT NULL DEFAULT false, 
-    username text NOT NULL,
-    todolist_id integer
-        NOT NULL
-        REFERENCES todolists(id)
-        ON DELETE CASCADE
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  done boolean NOT NULL DEFAULT false,
+  username text NOT NULL,
+  todolist_id integer
+    NOT NULL
+    REFERENCES todolists (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE users (
